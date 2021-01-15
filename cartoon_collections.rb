3801e5eb2601ef9e6806ddb2b167afe9023f3df7
Carly Gerber <carlyagerber@gmail.com>
2021-01-15 21:56:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,12 +3,9 @@ dwarves = ["Dopey", "Grumpy", "Bashful"]
 def greet_characters(array)
   array.each { |name|
     puts "Hello #{name.capitalize}!"
-    p name 
-    p array 
   }
 end 
 
-greet_characters(dwarves)
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
